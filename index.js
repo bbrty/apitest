baf05923054fdca0261c9f72abf78a80b7702cca
bbrty <88633188+bbrty@users.noreply.github.com>
2022-07-13 21:02:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('express')();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    
+    res.send('Hello World!');
+});
+app.listen(
+    port,
+    () => console.log(`Server running on port ${port}`)
+)
